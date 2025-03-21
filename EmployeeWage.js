d3a9c9d92a6958fe668dffa8714a9f5f4856d1e1
Aditya <adityasinghbtp123@gmail.com>
2025-03-21 14:42:01 +0530

    first commit

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,3 +11,27 @@ if(empCheck == Ispresent){
 else{
     console.log("Employee is Absent");
 }
+
+// UC2 (Calculate daily employee wage)
+
+let hours = Math.floor(Math.random() * 10) % 3;
+
+const isPartTime = 1;
+const isFullTime = 2;
+const partTime = 4;
+const fullTime = 8;
+const wagePerHour = 20;
+
+switch(hours){
+    case isPartTime:
+        hours = partTime;
+        break;
+    case isFullTime:
+        hours = fullTime;
+        break;
+    default:
+        hours = 0;
+}
+
+let wage = wagePerHour * hours;
+console.log("Daily wage:", wage);
