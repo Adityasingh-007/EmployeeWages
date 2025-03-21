c20e4449c80f18bb7f76f7447f17701e5a309c97
Aditya <adityasinghbtp123@gmail.com>
2025-03-21 14:35:38 +0530

    first commit

---
+++ EmployeeWage.js
@@ -0,0 +1,13 @@
+
+// UC1
+
+const Ispresent = 1;
+
+let empCheck = Math.floor(Math.random() * 10) % 2;
+
+if(empCheck == Ispresent){
+    console.log("Employee is Present");
+}
+else{
+    console.log("Employee is Absent");
+}
